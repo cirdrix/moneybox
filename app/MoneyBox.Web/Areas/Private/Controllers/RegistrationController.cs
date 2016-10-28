7350f9f5a6e2a2f74c82f98d65587c9ffd0415be
Alessandro Cirelli <cirelli.alessandro@gmail.com>
2016-10-28 22:35:21 +0200

    Add Private area registration

---
+++ app/MoneyBox.Web/Areas/Private/Controllers/RegistrationController.cs
@@ -0,0 +1,14 @@
+﻿namespace MoneyBox.Web.Areas.Private.Controllers
+{
+    using System.Web.Mvc;
+
+    [Authorize]
+    public class RegistrationController : Controller
+    {
+        // GET: Private/Registration
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
