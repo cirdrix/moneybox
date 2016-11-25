8528011357a6ff311216160909d9b4323598bc2a
Alessandro Cirelli <acirelli@absistemi.it>
2016-11-25 23:55:26 +0100

    Add import form and epplus library reference

--- app/MoneyBox.Web/Areas/Private/Controllers/RegistrationController.cs
+++ app/MoneyBox.Web/Areas/Private/Controllers/RegistrationController.cs
@@ -7,6 +7,7 @@
     using System.Web.Mvc;
 
     using MoneyBox.Domain;
+    using MoneyBox.Web.Areas.Private.Models;
 
     using Services;
 
@@ -61,67 +62,10 @@
             },
             JsonRequestBehavior.AllowGet);
         }
-    }
 
-    /// <summary>
-    /// Class that encapsulates most common parameters sent by DataTables plugin
-    /// </summary>
-    public class JQueryDataTableParamModel
-    {
-        /// <summary>
-        /// Request sequence number sent by DataTable,
-        /// same value must be returned in response
-        /// </summary>       
-        public string sEcho { get; set; }
-
-        /// <summary>
-        /// Text used for filtering
-        /// </summary>
-        public string sSearch { get; set; }
-
-        /// <summary>
-        /// Number of records that should be shown in table
-        /// </summary>
-        public int iDisplayLength { get; set; }
-
-        /// <summary>
-        /// First record that should be shown(used for paging)
-        /// </summary>
-        public int iDisplayStart { get; set; }
-
-        /// <summary>
-        /// Number of columns in table
-        /// </summary>
-        public int iColumns { get; set; }
-
-        /// <summary>
-        /// Number of columns that are used in sorting
-        /// </summary>
-        public int iSortingCols { get; set; }
-
-        /// <summary>
-        /// Comma separated list of column names
-        /// </summary>
-        public string sColumns { get; set; }
-
-        /// <summary>
-        /// First ordering column
-        /// </summary>
-        public int iSortCol_0 { get; set; }
-
-        /// <summary>
-        // First ordering column direction
-        /// </summary>
-        public string sSortDir_0 { get; set; }
-
-        public string[] FilteringColums
+        public ActionResult Import()
         {
-            get { return sColumns.Split(','); }
-        }
-
-        public string SearchTerms
-        {
-            get { return string.IsNullOrEmpty(sSearch) ? string.Empty : sSearch; }
+            return this.View();
         }
     }
 }
